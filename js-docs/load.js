74ded097a4703e22a69c1217e51a01af4e5aa373
Arias <agarias@ucsd.edu>
2023-08-01 17:28:49 -0700

    implemented the remote server, thus finishing the HW

--- js-docs/load.js
+++ js-docs/load.js
@@ -2,11 +2,11 @@ function init() {
     const loadLocalButton = document.getElementById('load-local-button');
     const loadRemoteButton = document.getElementById('load-remote-button');
 
-    loadLocalButton.addEventListener('click', function () {
+    loadLocalButton.addEventListener('click', function() {
         loadLocalData();
     });
 
-    loadRemoteButton.addEventListener('click', function () {
+    loadRemoteButton.addEventListener('click', function() {
         loadRemoteData();
     });
 }
@@ -24,7 +24,8 @@ function loadLocalData() {
     //clear existing content in container
     localCardsContainer.innerHTML = '';
 
-    for (let currentProjectCard = 0; currentProjectCard < localStorageData.length; currentProjectCard++) {
+    for (let currentProjectCard = 0; currentProjectCard < localStorageData
+        .length; currentProjectCard++) {
         let projectCard = document.createElement('project-card');
 
         projectCard.data = {
@@ -44,7 +45,56 @@ function loadLocalData() {
 
 }
 
+function populateRemoteContainer(remoteStorageData) {
+    if (remoteStorageData === null) {
+        return;
+    }
+
+    let remoteCardsContainer = document.getElementById("remote-storage-cards");
+
+    //clear existing content in container
+    remoteCardsContainer.innerHTML = '';
+
+    for (let currentProjectCard = 0; currentProjectCard < remoteStorageData
+        .length; currentProjectCard++) {
+        let projectCard = document.createElement('project-card');
+
+        projectCard.data = {
+            'title': remoteStorageData[currentProjectCard].title,
+            'imageSrc': remoteStorageData[currentProjectCard].imageSrc,
+            'imageAlt': remoteStorageData[currentProjectCard].imageAlt,
+            'description': remoteStorageData[currentProjectCard].description,
+            'linkHref': remoteStorageData[currentProjectCard].linkHref,
+            'linkTxt': remoteStorageData[currentProjectCard].linkTxt,
+            'author': remoteStorageData[currentProjectCard].author,
+            'date': remoteStorageData[currentProjectCard].date,
+            'status': remoteStorageData[currentProjectCard].status
+        };
+
+        remoteCardsContainer.append(projectCard);
+    }
+}
+
 function loadRemoteData() {
-    alert('clicked remote button!');
+    fetch('https://api.jsonbin.io/v3/b/64c99bb99d312622a38a3f24', {
+            method: 'GET',
+            headers: {
+                'X-Master-Key': '$2b$10$Z.LBmk3KSHsnlCf4lBVuruyhvOyc9dIoGhgs8GdpNliWY538BDuwi'
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("The Network Response was not ok!");
+            } else {
+                return response.json();
+            }
+
+        }).then(data => {
+            populateRemoteContainer(data.record);
+        }).catch(error => {
+            console.error(
+                'An Error Occured Fetching The Data From the Remote Server: ',
+                error);
+        });
 }
 window.addEventListener('DOMContentLoaded', init);
